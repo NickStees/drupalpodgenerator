b02c0ee8c55227330246ffd772e3fecd20495348
Nicholas Stees <nstees@gmail.com>
2023-01-10 23:42:14 -0500

    And a cloud function to fetch module details from drupal and convert from XML to json

--- functions/module/[module].js
+++ functions/module/[module].js
@@ -1,3 +1,30 @@
-export function onRequest(context) {
-  return new Response(JSON.stringify(context.params.module))
+import { convertXML } from 'simple-xml-to-json';
+export async function onRequest(context) {
+  // context.params.module is the module name
+  const module = context.params.module;
+
+  // fetch() module details from https://updates.drupal.org/release-history/${module}/current
+  var url = `https://updates.drupal.org/release-history/${module}/current`
+
+  // fetch XML content
+  const res = await fetch(url, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/xml',
+    },
+  })
+
+  // check if the response is ok
+  if (!res.ok) {
+    return new Response(res.statusText, { status: res.status })
+  }
+  const txt = await res.text();
+
+  const details = convertXML(txt);
+
+  return new Response(JSON.stringify(details))
+
+
+
+
 }
